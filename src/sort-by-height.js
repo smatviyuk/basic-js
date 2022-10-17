d2eb12ed4f0f0c3b1ea399948ff686e10e4da10f
smatviyuk <smatviyuk@bk.ru>
2022-10-17 17:24:32 +0300

    fix: bag fixed

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -21,7 +21,7 @@ function sortByHeight(arr) {
   }).sort((a,b) => a-b)
   for (let i = 0; i < arr.length; i++) {
     if(arr[i] != -1) {
-      arr[i] = arr[index];
+      arr[i] = a[index];
       index++;
     }
   }
